d0b64d83906446fdc923a73db6a1cb7a531e63b7
lorenzo-rovigatti <lorenzo.rovigatti@gmail.com>
2018-04-20 16:48:00 +0200

    Added cmake support

---
+++ System.cpp
@@ -0,0 +1,15 @@
+/*
+ * System.cpp
+ *
+ *  Created on: 20 Apr 2018
+ *      Author: lorenzo
+ */
+
+#include "System.h"
+
+System::System() {
+}
+
+System::~System() {
+}
+
---
+++ System.h
@@ -0,0 +1,17 @@
+/*
+ * System.h
+ *
+ *  Created on: 20 Apr 2018
+ *      Author: lorenzo
+ */
+
+#ifndef SYSTEM_H_
+#define SYSTEM_H_
+
+class System {
+public:
+	System();
+	virtual ~System();
+};
+
+#endif /* SYSTEM_H_ */
